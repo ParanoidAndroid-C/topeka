4098faf184061be1e8c4b29bd84cb1384e26c102
Mariami Lomtadze <71402417+ParanoidAndroid-C@users.noreply.github.com>
2021-02-03 19:42:10 -0800

    Delete config.py

--- Python/config.py
+++
@@ -1,9 +0,0 @@
-# Spotify
-CLIENT_ID = '8a43522300444ecab7fe022d8f34af26'
-CLIENT_SECRET = 'eabe25d20fe04d13935c98fa459dedef'
-
-# Firebase
-FB_API_KEY = "AIzaSyCLOF_XZfM2ajE3XxZ8Fc_VYOQGsqSQF_o"
-FB_AUTH_DOMAIN = "fridge-dce71.firebaseapp.com"
-FB_DATABASE_URL = "https://fridge-dce71-default-rtdb.firebaseio.com/"
-FB_STORAGE_URL = "fridge-dce71.appspot.com"
